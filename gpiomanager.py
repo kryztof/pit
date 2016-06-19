d4a131ac709f488edccd5d43e97762aa53bf0166
kryztof <kdewilde@gmail.com>
2016-06-19 14:20:02 +0200

    removing gpio event detection on deletion

--- gpiomanager.py
+++ gpiomanager.py
@@ -19,16 +19,23 @@ class GpioManager:
     except:
       print("No module RPi.GPIO")
 
+  def __del__(self):
+    GPIO.remove_event_detect(self.channels[0])
+    GPIO.remove_event_detect(self.channels[1])
+
   def make_callbacks(self):
-    GPIO.add_event_detect(self.channels[0], GPIO.FALLING, callback = self.callback, bouncetime=100)
-    GPIO.add_event_detect(self.channels[1], GPIO.FALLING, callback = self.callback, bouncetime=100)
+    dbgprint("1")
+    GPIO.add_event_detect(self.channels[0], GPIO.FALLING, callback = self.callback, bouncetime=200)
+    dbgprint("2")
+    GPIO.add_event_detect(self.channels[1], GPIO.FALLING, callback = self.callback, bouncetime=200)
+    dbgprint("3")
 
   def callback(self,channel):
     #dbgprint("gettting button input")
-    if channel == 24 :
+    if channel == 26 :
       buttonevent = pygame.event.Event(pygame.USEREVENT+4)
       pygame.event.post(buttonevent)
-    elif channel == 26 :
+    elif channel == 24 :
       buttonevent = pygame.event.Event(pygame.USEREVENT+5)
       pygame.event.post(buttonevent)
 
